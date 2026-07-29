c4f09fda10822b068d38d13bc8b50e3ebc7d568f
GautierPicon <bt2ay5d9@anonaddy.com>
2026-07-30 01:17:41 +0200

    feat: add console easter egg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { displayConsoleArt } from '$lib/ConsoleArt';
 
 	let { children } = $props();
+
+	displayConsoleArt();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
