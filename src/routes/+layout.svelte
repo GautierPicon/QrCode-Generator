081091dddd08aa65e644c66dd8fb5fe828d69cd2
GautierPicon <bt2ay5d9@anonaddy.com>
2026-07-30 10:09:04 +0200

    fix: guard easter egg with browser check to avoid server console log

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { browser } from '$app/environment';
 	import { displayConsoleArt } from '../scripts/ConsoleArt';
 
 	let { children } = $props();
 
-	displayConsoleArt();
+	if (browser) displayConsoleArt();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
